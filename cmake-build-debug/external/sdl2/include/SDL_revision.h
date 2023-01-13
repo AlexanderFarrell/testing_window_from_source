606926dfb338995ba4833e0c7dc7f06d6dc29079
Alexander Farrell <info@alexanderfarrell.com>
2023-01-12 19:15:53 -0800

    Including build files

---
+++ cmake-build-debug/external/sdl2/include/SDL_revision.h
@@ -0,0 +1,6 @@
+#define SDL_REVISION "https://github.com/AlexanderFarrell/testing_window_from_source.git@d2607c2182aebcb4dc63d8474124be32cadfa84a"
+#define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
